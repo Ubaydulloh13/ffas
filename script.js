fe8db28f8286a0cea03603719f76ce525804c94f
Ubaydulloh13 <samushakirov@gmail.com>
2025-08-04 12:20:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//+plus
+//-minus
+//*ko'paytirish
+// /bo'lish
+// %qoldiq bo'lish
+// 2**3=8 daraja
+
+let randomson=Math.floor(Math.random()*90+10);
+alert("sizda random son chiqishni boshlaydi: 😊");
+alert("random orqali chiqqan son:" +randomson);
+confirm("Biz bu sonni 2ga ko'paytirib bo'lmoqchimiz.");
+alert(`Random orqali chiqqan son ${randomson} \n Bu sonni 2ga ko'paytirdik: ${randomson*2}
+    \n Bu sonni 2ga bo'ldik: ${randomson/2} \n Bu sonni 3ga qoldili bo'ldik: ${randomson%3}`)
